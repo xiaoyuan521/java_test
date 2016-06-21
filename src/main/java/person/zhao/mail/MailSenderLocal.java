4f271d7bab1c65c8d643b2904e92f3b864181411
yisurenali@aliyun.com <yisurenali@aliyun.com>
2016-06-21 11:03:10 +0800

    add mail sender example

--- src/main/java/person/zhao/mail/MailSenderLocal.java
+++ src/main/java/person/zhao/mail/MailSenderLocal.java
@@ -1,7 +1,5 @@
 package person.zhao.mail;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -12,47 +10,29 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class MailSender {
+/**
+ * 连接本地的127.0.0.1的smtp（25端口）进行邮件发送 无需密码
+ * 
+ * @author zhao_hongsheng
+ */
+public class MailSenderLocal {
 
     private static String RECIPIENT = "zhao_hongsheng@microad-tech.com";
-    private Properties passProps = null;
 
-    public MailSender() {
-        readProps();
+    public MailSenderLocal() {
     }
 
     private void sendmail(String[] to, String subject, String body) {
+        String userName = "zhao_hongsheng@microad-tech.com";
+
         Properties props = System.getProperties();
 
-        // =========== 263 ===============
-        // 非加密，使用25 端口
         // HOST and PORT
-        // String host = "smtp.263.net";
-        // props.put("mail.smtp.host", host);
-        // props.put("mail.smtp.port", "25");
-        //
-        // // USER_NAME and PASSWORD
-        // String userName = passProps.getProperty("263.user.name");
-        // String password = passProps.getProperty("263.user.pass");
-        // props.put("mail.smtp.auth", "true");
-        // props.put("mail.smtp.user", userName);
-        // props.put("mail.smtp.password", password);
-
-        // =========== qq ===============
-        // 加密，使用587端口，具体端口还需要根据smtp服务器来设定
-        // HOST and PORT
-        String host = "smtp.qq.com";
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.port", "587");
+        String host = "127.0.0.1";
+        props.put("mail.smtp.port", "25");
         props.put("mail.smtp.host", host);
+        props.put("mail.smtp.auth", "false");
 
-        // USER_NAME and PASSWORD
-        String userName = passProps.getProperty("qq.user.name");
-        String password = passProps.getProperty("qq.user.pass");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.user", userName);
-        props.put("mail.smtp.password", password);
-        
         Session session = Session.getDefaultInstance(props);
         MimeMessage message = new MimeMessage(session);
 
@@ -76,23 +56,13 @@ public class MailSender {
 
             // send the mail
             Transport transport = session.getTransport("smtp");
-            transport.connect(host, userName, password);
+            transport.connect(host, userName);
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
         } catch (AddressException ae) {
             ae.printStackTrace();
         } catch (MessagingException me) {
             me.printStackTrace();
-        }
-    }
-
-    private void readProps() {
-        passProps = new Properties();
-        InputStream in = MailSender.class.getClassLoader().getResourceAsStream("pass.properties");
-        try {
-            passProps.load(in);
-        } catch (IOException e) {
-            throw new RuntimeException("Error, when read properties file !");
         }
     }
 
@@ -104,6 +74,6 @@ public class MailSender {
     }
 
     public static void main(String[] args) {
-        new MailSender().run();
+        new MailSenderLocal().run();
     }
 }
