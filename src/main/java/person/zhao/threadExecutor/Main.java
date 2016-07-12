aebb3bfa214afa63a92579d428e2d4d6a5b94f18
zhao_hongsheng <zhao_hongsheng@microad-tech.com>
2016-07-12 16:56:03 +0800

    add thread executor

---
+++ src/main/java/person/zhao/threadExecutor/Main.java
@@ -0,0 +1,40 @@
+package person.zhao.threadExecutor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    private static int CORE_THREAD_NUM = 10;
+    private static int MAX_THREAD_NUM = 13;
+    private static Long KEEP_ALIVE_TIME = 0L;
+
+    public void p() {
+        ExecutorService service =
+                new ThreadPoolExecutor(CORE_THREAD_NUM,
+                        MAX_THREAD_NUM,
+                        KEEP_ALIVE_TIME,
+                        TimeUnit.SECONDS,
+                        new SynchronousQueue<Runnable>());
+        
+        for (int i = 0; i < 100; i++) {
+            Runnable runnable = new ProducerThread(service, "thread - " + i);
+            service.submit(runnable);
+            sleep(10);
+        }
+    }
+    
+    public void sleep(int milisec){
+        try {
+            Thread.sleep(milisec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().p();
+    }
+}
---
+++ src/main/java/person/zhao/threadExecutor/ProducerThread.java
@@ -0,0 +1,29 @@
+package person.zhao.threadExecutor;
+
+import java.util.concurrent.ExecutorService;
+
+public class ProducerThread implements Runnable{
+
+    private String name = null;
+
+    public ProducerThread(ExecutorService executeService, String name) {
+        super();
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(String.format("I am thread [%s]", this.name));
+        sleep(100);
+        
+    }
+    
+    public void sleep(int milisec){
+        try {
+            Thread.sleep(milisec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
